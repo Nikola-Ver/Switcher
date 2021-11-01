6c6816be088dcfbe45ee13fa0540822f521f897e
Nikolay Vereshchagin <nikolveresh@gmail.com>
2021-11-01 03:13:29 +0300

    add: text info

--- swgit.bat
+++ swgit.bat
@@ -4,10 +4,12 @@ IF "%1" == "job" (
     IF "%2" == "tng" (
         git config --global user.name "Nikolay Vereshchagin"
         git config --global user.email nikolai@triplenext.com
+        ECHO SWITCHED TO TANGIBLEE
     )
 ) ELSE IF "%1" == "me"  (
     git config --global user.name "Nikolay Vereshchagin"
     git config --global user.email nikolveresh@gmail.com
+    ECHO SWITCHED TO ME
 )
 
 @ECHO ON
